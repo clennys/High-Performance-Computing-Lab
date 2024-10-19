ab7bf737ff898e92bcf36342628259a33c3893d2
Dennys Mike Huber <huberde@icslogin01.cluster.net>
2024-10-19 13:21:23 +0200

    ran all the exercise on the rosa cluster

--- project02/code/hist/run_hist_scaling.sh
+++ project02/code/hist/run_hist_scaling.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=hist_scaling-%j.out # Output file
 #SBATCH --error=hist_scaling-%j.err  # Error file
 #SBATCH --ntasks=1                   # Number of tasks
-#SBATCH --cpus-per-task=32           # Number of CPUs per task
+#SBATCH --cpus-per-task=20          # Number of CPUs per task
 #SBATCH --mem-per-cpu=1024           # Memory per CPU
 #SBATCH --time=00:05:00              # Wall clock time limit
 
